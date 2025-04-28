8ac729981c8c58e012ce4ea517567ebc2b68e49d
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-29 00:59:30 +0300

    Initial Commit

---
+++ KisilerList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerList
+//
+//  Created by Ömer on 29.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KisilerListTests/KisilerListTests.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerListTests.swift
+//  KisilerListTests
+//
+//  Created by Ömer on 29.04.2025.
+//
+
+import Testing
+@testable import KisilerList
+
+struct KisilerListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
